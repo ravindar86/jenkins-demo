dfd31d4c47f130862543e001adced09c5ffb5bea
ravindar86 <ravindar.mca@gmail.com>
2021-11-17 08:16:17 +0530

    Added Logger post test case execution

--- src/test/java/com/example/jenkinsdemo/JenkinsDemoApplicationTests.java
+++ src/test/java/com/example/jenkinsdemo/JenkinsDemoApplicationTests.java
@@ -1,19 +1,22 @@
 package com.example.jenkinsdemo;
 
 import org.junit.jupiter.api.Test;
-import org.junit.platform.commons.logging.Logger;
-import org.junit.platform.commons.logging.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class JenkinsDemoApplicationTests {
 
-    private static Logger logger= LoggerFactory.getLogger(JenkinsDemoApplication.class);
+    private static final Logger logger= LoggerFactory.getLogger(JenkinsDemoApplication.class);
     @Test
     void contextLoads() {
+        logger.info("Test Case executing...");
         assertEquals(true,true);
+        logger.info("Test Case finished executing...");
     }
 
 }
